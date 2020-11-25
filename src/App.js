3e2af27ff36a7c6fba075e1a1706d91659195be3
tanguy <lefloch.ta@gmail.com>
2020-11-25 20:28:14 +0100

    chartjs first version

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import React from "react";
 
+import {Radar} from 'react-chartjs-2';
+
 import logo from './logo.svg';
 import './App.css';
 
 import Question from './Question';
 import data from './data/questions.json'
 
+// Preparing the categories
+let categories = data.map(
+    (question) => Object.keys(question.answers).map(
+        (key) => Object.keys(question.answers[key])
+    )
+);
+categories = [].concat.apply([], categories);
+categories = [].concat.apply([], categories);
+const allCategories = [...new Set(categories)];
+
+function objectValuesSum(obj) {
+    return Object.keys(obj).reduce(
+        (sum, key) => sum + parseFloat(obj[key] || 0),
+        0
+    );
+}
+
 const initialState = {
-    score: 0,
+    score: allCategories.reduce((o, key) => ({...o, [key]: 0}), {}),
     currentQuestionIndex: 0
 };
 
@@ -20,9 +39,13 @@ class App extends React.Component {
         this.state = initialState;
     }
 
-    handleAnswerClick(scoreChangeValue) {
+    handleAnswerClick(scoreChanges) {
+        let newScore = JSON.parse(JSON.stringify(this.state.score));
+        for (const category of Object.keys(scoreChanges))
+            newScore[category] += scoreChanges[category];
+
         this.setState({
-            score: this.state.score + scoreChangeValue,
+            score: newScore,
             currentQuestionIndex: this.state.currentQuestionIndex + 1
         })
     }
@@ -30,21 +53,42 @@ class App extends React.Component {
     render() {
         let display = "";
 
-        if (this.state.currentQuestionIndex === data.length)
-            display = <div>C'est fini, ton score est {this.state.score} !
-                <button className="link-button" onClick={() => {this.setState(initialState)}}>
-                    <br/>
+        if (this.state.currentQuestionIndex === data.length) {
+            let scoreValues = allCategories.map((category) => this.state.score[category]);
+
+            display = <div><br/>C'est fini, ton score est {objectValuesSum(this.state.score)} !
+                <Radar
+                    data={{
+                        labels: allCategories,
+                        datasets: [{
+                            data: scoreValues,
+                            backgroundColor: "red",
+                            fill: false,
+                            borderColor: "red"
+                        }]
+                    }}
+                    width={500} height={500}
+                    options={
+                        {
+                            "legend": {"display": false},
+                            "scale": {"ticks": {"display": true, "min": -20}}
+                        }
+                    }
+                />
+                <button className="link-button" onClick={() => {
+                    this.setState(initialState)
+                }}>
                     Le refaire.
                 </button>
-        </div>;
-        else
+            </div>;
+        } else
             display = <Question question={data[this.state.currentQuestionIndex]}
                                 onClickButton={this.handleAnswerClick}/>;
         return (
             <div className="App">
                 <header className="App-header">
                     <img src={logo} className="App-logo" alt="logo"/>
-                        {display}
+                    {display}
                 </header>
             </div>
         );
