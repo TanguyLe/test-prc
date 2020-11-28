e2ab734bff3b366c9d24d800eaf2192bccb1d48e
tanguy <lefloch.ta@gmail.com>
2020-11-28 13:55:44 +0100

    Footer improvements

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,16 @@
+import React from "react";
+
 const Footer = () => {
-  return <div>
-      Icons made by <a href="https://www.flaticon.com/authors/bqlqn" title="bqlqn">bqlqn
-  </a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
+  return <div className="Footer">
+                  <a href="www.test-prc.fr">www.test-prc.fr</a> ne sauvegarde ou ne
+                  partage aucune de vos infomations personnelles ou données de navigation.
+                  <a href="https://github.com/TanguyLe/test-prc">
+                  <img src="GitHub-Mark-32px.png" alt="Github" style={{width: "20px", height: "20px"}}/>
+                </a>
+                  Favicon made
+                  by <a href="https://www.flaticon.com/authors/bqlqn" title="bqlqn">
+                      bqlqn
+                  </a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>.
   </div>
 };
 
