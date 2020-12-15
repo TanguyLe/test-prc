cb844ffee7717a8b28a4422c971055f9aa393c3a
tanguy <lefloch.ta@gmail.com>
2020-12-15 17:03:09 +0100

    Add whatsapp button

--- src/components/EndDisplay.jsx
+++ src/components/EndDisplay.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
 import { Button } from "react-bootstrap";
-
 import {Radar} from 'react-chartjs-2';
+import Rainbow from 'rainbowvis.js';
+
 import {arraySum, getScoresStats} from "../utils";
 
-const Rainbow = require('rainbowvis.js');
 
 const SENTENCES_SCORE = {
     1: "Tu as fait un score très, très bas, tu es es sûr d'être vivant ?",
@@ -18,6 +18,23 @@ const SENTENCES_SCORE = {
 };
 
 const FONT_SIZE = "calc(10px + 2vmin)";
+
+const getWhatsAppShareButton = (text) => {
+    return <a href={"https://api.whatsapp.com/send?&text=" + text}>
+        <img src="WhatsApp_Logo_1.png" alt="WhatsApp" style={{width: "50px", height: "50px"}}/>
+    </a>
+};
+const getTwitterShareButton = (text) => {
+    return [
+        <a
+            href="https://twitter.com/share?ref_src=twsrc%5Etfw"
+            className="twitter-share-button"
+            data-show-count="false">
+            Tweet
+        </a>,
+        <script async src = "https://platform.twitter.com/widgets.js" charSet = "utf-8" > </script>
+    ]
+};
 
 
 class EndDisplay extends React.Component {
@@ -38,6 +55,11 @@ class EndDisplay extends React.Component {
         while (Object.keys(SENTENCES_SCORE)[currentSentenceIndex] < scorePercent)
             currentSentenceIndex += 1;
         currentSentenceIndex = Object.keys(SENTENCES_SCORE)[currentSentenceIndex - 1];
+
+        const shareText = (
+            `Je viens de finir mon test prc sur http://www.test-prc.fr/ avec un score de ${score}, `
+            + "à ton tour de voir si tu as été respectueux du Covid !"
+        );
 
         const scoreDiv = <div style={{"color":  '#' + this.rainbow.colourAt(score), "display": "inline"}}>{score}</div>;
         return <div>
@@ -68,6 +90,7 @@ class EndDisplay extends React.Component {
                         }
                     }
                 />
+                {getWhatsAppShareButton(shareText)}
                 <Button onClick={this.props.reset}>
                     Encore !
                 </Button>
